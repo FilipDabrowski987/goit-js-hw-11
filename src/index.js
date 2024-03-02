4b4be2c1db1bb4ff19cfa52a6b7b2e6d5adcc352
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-03-02 17:47:09 +0100

    update

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const input = form.querySelector('input[type="text"][name="searchQuery"]');
+const button = form.querySelector('button[type="submit"]');
