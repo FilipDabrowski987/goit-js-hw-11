f2625330d8772e2b62e0810136168e3ab3fffef1
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-03-02 18:02:44 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
+const form = document.querySelector('#search-form');
 const input = form.querySelector('input[type="text"][name="searchQuery"]');
 const button = form.querySelector('button[type="submit"]');
+
+const fetchImages = () => { 
+    axios.defaults.headers.common["x-api-key"] = '42664438-fd58fde2f94660d61e5943804';
+    return axios
+        .get(`https://pixabay.com/api/`)
+        .then(response => response.data);
+};
