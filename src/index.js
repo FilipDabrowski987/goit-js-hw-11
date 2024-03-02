433aa93026af11301a0f85d64624c3255fc30f88
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-03-02 19:49:36 +0100

    update

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ const form = document.querySelector('#search-form');
 const input = form.querySelector('input[type="text"][name="searchQuery"]');
 const button = form.querySelector('button[type="submit"]');
 
-const fetchImages = () => {
+//const qInput = input.value;
+
+async function fetchImages() {
+    const searchParams = new URLSearchParams({
+        _key: '42664438-fd58fde2f94660d61e5943804',
+        _q: input.value.split(' ').join('+'),
+        _image_type: 'photo',
+        _orientation: 'horizontal',
+        _safesearch: 'true',
+    });
        // axios.defaults.headers.common["x-api-key"] = '42664438-fd58fde2f94660d61e5943804';
         return axios
         .get(`https://pixabay.com/api/?key=42664438-fd58fde2f94660d61e5943804`)
         .then(response => response.data)
-        .then(console.log('obraz'));
+        .then(console.log('obraz'))
+        .then(console.log(searchParams));
 };
 
 form.addEventListener('submit', (event) => {
