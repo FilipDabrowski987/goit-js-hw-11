2ecc1b3f9c21a4692ef615a31e3d970df1adcc69
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-03-02 23:36:29 +0100

    update

--- src/index.js
+++ src/index.js
@@ -8,17 +8,17 @@ const gallery = document.querySelector('.gallery');
 
 async function fetchImages() {
     const searchParams = new URLSearchParams({
-        _key: '42664438-fd58fde2f94660d61e5943804',
-        _q: input.value.split(' ').join('+'),
-        _image_type: 'photo',
-        _orientation: 'horizontal',
-        _safesearch: 'true',
+        key: '42664438-fd58fde2f94660d61e5943804',
+        q: input.value.split(' ').join('+'),
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
     });
 
     try {
         // axios.defaults.headers.common["x-api-key"] = '42664438-fd58fde2f94660d61e5943804';
         const response = await axios
-            .get(`https://pixabay.com/api/?key=42664438-fd58fde2f94660d61e5943804&${searchParams}`);
+            .get(`https://pixabay.com/api/?${searchParams}`);
         const images = response.data.hits.map(image => ({
             webformatURL: image.webformatURL,
             largeImageURL: image.largeImageURL,
@@ -43,7 +43,7 @@ async function fetchImages() {
         `);
 
         gallery.insertAdjacentHTML('beforeend', imageCard.join(''));
-        //console.log(response.data);
+        console.log(response.data);
         //console.log(searchParams);
         //.then(response => response.data)
         //.then(console.log(searchParams))
@@ -71,22 +71,3 @@ form.addEventListener('submit', (event) => {
   //error.classList.remove('hidden');
   //loader.classList.add('hidden');
 //};
-
-
-//<div class="photo-card">
-  //<img src="" alt="" loading="lazy" />
-  //<div class="info">
-    //<p class="info-item">
-      //<b>Likes</b>
-    //</p>
-    //<p class="info-item">
-      //<b>Views</b>
-    //</p>
-    //<p class="info-item">
-      //<b>Comments</b>
-    //</p>
-    //<p class="info-item">
-      //<b>Downloads</b>
-    //</p>
-  //</div>
-//</div>
