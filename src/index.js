4c185c65ee67c7fe71c3b6c4ace593e8cf087c44
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-03-02 18:16:51 +0100

    update

--- src/index.js
+++ src/index.js
@@ -5,9 +5,23 @@ const form = document.querySelector('#search-form');
 const input = form.querySelector('input[type="text"][name="searchQuery"]');
 const button = form.querySelector('button[type="submit"]');
 
-const fetchImages = () => { 
-    axios.defaults.headers.common["x-api-key"] = '42664438-fd58fde2f94660d61e5943804';
-    return axios
-        .get(`https://pixabay.com/api/`)
-        .then(response => response.data);
+const fetchImages = () => {
+        axios.defaults.headers.common["x-api-key"] = '42664438-fd58fde2f94660d61e5943804';
+        return axios
+            .get(`https://pixabay.com/api/`)
+            .then(response => response.data);
 };
+
+try {
+    fetchImages()
+    //.then(data => renderSelect(data))
+    //.catch(showError);
+} catch (error) {
+  console.error(error);
+}
+
+//function showError(errorMessage) {
+  //console.error(errorMessage);
+  //error.classList.remove('hidden');
+  //loader.classList.add('hidden');
+//};
